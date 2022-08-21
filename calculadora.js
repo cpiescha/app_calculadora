b2cf59dd8059862bdcc1ccd8a9cee7c3d621f068
cpiescha <cpiescha99@gmail.com>
2022-08-21 16:13:11 -0500

    agregando codigo

---
+++ calculadora.js
@@ -0,0 +1,55 @@
+console.log('Aplicacion calculadora');
+
+function sumar(){
+    const forma = document.getElementById('forma');
+    let  operandoA = forma['operandoA'];
+    let  operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    if(isNaN(resultado)){
+        resultado = 'debes ingresar un numero para realizar la operacion';
+        console.log('no ingresa numero')
+    }
+    document.getElementById('resultado').innerHTML=`Resultado: ${resultado}`
+    console.log(`Resultado: ${resultado}`);
+    
+}
+function restar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
+    if(isNaN(resultado)){
+        resultado = 'debes ingresar un numero para realizar la operacion';
+        console.log('no ingresa numero')
+    }
+    document.getElementById('resultado').innerHTML=`Resultado: ${resultado}`
+    console.log(`Resultado: ${resultado}`);
+} 
+function multiplicar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
+    if(isNaN(resultado)){
+        resultado = 'debes ingresar un numero para realizar la operacion';
+        console.log('no ingresa numero')
+    }
+    document.getElementById('resultado').innerHTML=`Resultado: ${resultado}`
+    console.log(`Resultado: ${resultado}`);
+} 
+
+function dividir(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value)/parseInt(operandoB.value);
+    if(isNaN(resultado)){
+        resultado = 'debes ingresar un numero para realizar la operacion';
+        console.log('no ingresa numero')
+    }
+    document.getElementById('resultado').innerHTML=`Resultado: ${resultado}`
+    console.log(`Resultado: ${resultado}`);
+}     
+
+  
+ 
